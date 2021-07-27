8ee06fbbd6d7e23d0ba1b6bf4111b362ebca5cd9
Anuraag <princeanurag2011@gmail.com>
2021-07-27 22:20:55 +0530

    Update StudentController.java
    updated Controller

--- src/main/java/com/college/StudentController.java
+++ src/main/java/com/college/StudentController.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.college.db.StudentDB;
@@ -18,15 +24,15 @@ public class StudentController {
 		return "Hello";
 	}
 
-	@GetMapping("/getstudents")
-	public List<Student> getStudents() {
+	@GetMapping("/getallstudents")
+	public List<Student> getAllStudents() {
 
 		List<Student> studentList = new ArrayList<Student>();
 
 		try {
 			StudentDB studentDB = new StudentDB();
 
-			studentList = studentDB.fetchStudentDetials();
+			studentList = studentDB.fetchAllStudentDetials();
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -35,6 +41,51 @@ public class StudentController {
 
 		return studentList;
 
+	}
+	
+	
+	@GetMapping("/getstudent")
+	public Student getStudent(@RequestParam("id") int id) {
+
+		Student student = null;
+		System.out.println("inside getstudent method:"+id);
+		try {
+			StudentDB studentDB = new StudentDB();
+		   // int idnum= Integer.parseInt(id);
+			student = studentDB.fetchSpecificStudentData(id);		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Unable to fetch student details.!");
+		}
+
+		return student;
+
+	}
+	
+	@PostMapping("/createstudent")
+	//@RequestMapping(value = "/products", method = RequestMethod.POST)
+	public String createStudentData(@RequestBody Student student) {
+		
+		System.out.println("Inside post method..!");
+		String status = null;
+		try {
+			System.out.println(student.getId());
+			System.out.println(student.getName());
+			System.out.println(student.getAddress());
+			
+			StudentDB studentDB = new StudentDB();
+			status = studentDB.CreateStudentDetails(student);
+						
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			status = "Unable to insert student data";
+		}
+		
+		
+		return status;
 	}
 
 }
